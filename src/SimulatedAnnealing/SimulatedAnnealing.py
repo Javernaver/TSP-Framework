0d5a1698564d36df0819dd4db34952aba1d4cab5
Javernaver <Javernaver@gmail.com>
2021-07-30 23:49:46 -0400

    v0.00001

--- src/SimulatedAnnealing/SimulatedAnnealing.py
+++ src/SimulatedAnnealing/SimulatedAnnealing.py
@@ -1,18 +1,18 @@
 from enum import Enum
 
+class CoolingType(Enum):
+    """Esquemas de enfriamiento disponibles 
+
+    GEOMETRIC: t = t * alpha
+    LINEAR: t = t * (1 - (evaluation / max_evaluations))
+    LOG: t = t * alpha * 1/ln(evaluation + 1)
+    """
+    GEOMETRIC = 'GEOMETRIC'
+    LINEAR = 'LINEAR'
+    LOG = 'LOG'
 
 class SimulatedAnnealing():
 
-    class CoolingType(Enum):
-        """Esquemas de enfriamiento disponibles 
-
-        GEOMETRIC: t = t * alpha
-        LINEAR: t = t * (1 - (evaluation / max_evaluations))
-        LOG: t = t * alpha * 1/ln(evaluation + 1)
-        """
-        GEOMETRIC = 'GEOMETRIC'
-        LINEAR = 'LINEAR'
-        LOG = 'LOG'
 
 
     def __init__(self) -> None:
