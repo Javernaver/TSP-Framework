356f54fa26617e0d9ad75d32816e27902ef1732f
Javernaver <Javernaver@gmail.com>
2021-08-03 21:41:28 -0400

    avance sa

--- src/SimulatedAnnealing/SimulatedAnnealing.py
+++ src/SimulatedAnnealing/SimulatedAnnealing.py
@@ -1,8 +1,27 @@
-from src.AlgorithmsOptions import AlgorithmsOptions
+from src.Utilities import bcolors
+from src.AlgorithmsOptions import AlgorithmsOptions, TSPMove, CoolingType, InitialSolution
+from src.TSP import TSP
+from src.Tour import Tour
 
 class SimulatedAnnealing():
+    
+    # Problema 
+    problem = TSP
+    # Esquema de enfriamiento
+    cooling = CoolingType
+    # Tipo de movimiento
+    move_type = TSPMove
+    # Parametro alfa para el esquema de enfriamiento geometrico
+    alpha = 0.0
+    # Mejor tour 
+    best_tour = Tour
 
+    def __init__(self, problem: TSP, options: AlgorithmsOptions) -> None:
+        
+        self.problem = problem
+        self.cooling = options.cooling
+        self.move_type = options.move
+        self.alpha = options.alpha
+        self.best_tour = Tour(problem=self.problem, initial_sol=InitialSolution.RANDOM)
 
-
-    def __init__(self) -> None:
-        pass
+        print(f"{bcolors.HEADER}\nIniciando Simulated Annealing...{bcolors.ENDC}")
