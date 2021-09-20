4b6c472c797c2cf02cff2e0c06e65c77646159b4
Javernaver <Javernaver@gmail.com>
2021-09-20 01:44:21 -0300

    avance ga

--- src/Algorithms/GeneticAlgorithm.py
+++ src/Algorithms/GeneticAlgorithm.py
@@ -1,8 +1,9 @@
 import csv
 from datetime import datetime
 from pathlib import Path
-from src.Algorithms.Population import Population
+from timeit import default_timer as timer
 
+from src.Algorithms.Population import Population
 from src.Utilities import bcolors
 from src.Tour import Tour
 from src.TSP import TSP
@@ -11,37 +12,41 @@ from src.AlgorithmsOptions import AlgorithmsOptions, SelectionStrategy, Selectio
 
 class GeneticAlgorithm():
 
-    problem: TSP # Problema 	
-    
-    pop_size = 10 # Parametro tamaño de la poblacion 	
-    
-    offspring_size = 20	# Cantidad de hijos
-     
-    pselection_type = SelectionType.RANDOM # Seleccion de padres
-    
-    crossover_type = CrossoverType.OX # Cruzamiento 	
-     
-    mutation_type = MutationType.SWAP # Mutacion	
-     
-    mutation_prob = 0.2 # Probabilidad de mutacion
-     
-    selection_strategy = SelectionStrategy.MULAMBDA # Estrategia de seleccion de la nueva poblacion
-    
-    gselection_type = SelectionType.RANDOM # Tipo de seleccion de la poblacion
-    
-    elitism = 0 # Soluciones seleccionadas por elitismo 
-
-    best_tour :Tour # Mejor tour 
-    
-    total_time = 0.0 # tiempo de ejecucion de Algoritmo Genetico
-    
-    iterations = 1 # numero de iteraciones
-    
-    evaluations = 1 # numero de evaluaciones
-
-    options: AlgorithmsOptions # Opciones
-
     def __init__(self, options: AlgorithmsOptions = None, problem: TSP = None) -> None:
+
+        # Atributos de instancia
+        self.problem: TSP # Problema 	
+    
+        self.pop_size = 10 # Parametro tamaño de la poblacion 	
+        
+        self.offspring_size = 20	# Cantidad de hijos
+        
+        self.pselection_type = SelectionType.RANDOM # Seleccion de padres
+        
+        self.crossover_type = CrossoverType.OX # Cruzamiento 	
+        
+        self.mutation_type = MutationType.SWAP # Mutacion	
+        
+        self.mutation_prob = 0.2 # Probabilidad de mutacion
+        
+        self.selection_strategy = SelectionStrategy.MULAMBDA # Estrategia de seleccion de la nueva poblacion
+        
+        self.gselection_type = SelectionType.RANDOM # Tipo de seleccion de la poblacion
+        
+        self.elitism = 0 # Soluciones seleccionadas por elitismo 
+
+        self.best_tour: Tour = None # Mejor tour 
+        
+        self.total_time = 0.0 # tiempo de ejecucion de Algoritmo Genetico
+        
+        self.iterations = 1 # numero de iteraciones
+        
+        self.evaluations = 1 # numero de evaluaciones
+
+        self.options: AlgorithmsOptions # Opciones
+
+        self.total_time = 0.0 # tiempo de ejecucion de Algoritmo Genetico
+
         # Si por el objeto con las opciones no es enviado al iniciar la clase
         if not options:
             self.options = AlgorithmsOptions()
@@ -53,14 +58,21 @@ class GeneticAlgorithm():
         else:
             self.problem = problem
 
-        self.pop_size = self.options.pop_size
-        self.offspring_size = self.options.offspring_size
-        self.pselection_type = self.options.pselection_type
-        self.crossover_type = self.options.crossover_type
-        self.mutation_type = self.options.mutation_type
-        self.mutation_prob = self.options.mutation_prob
-        self.selection_strategy = self.options.selection_strategy
-        self.gselection_type = self.options.gselection_type
+        self.pop_size = self.options.pop_size # Parametro tamaño de la poblacion 
+
+        self.offspring_size = self.options.offspring_size # Cantidad de hijos
+
+        self.pselection_type = self.options.pselection_type # Seleccion de padres
+
+        self.crossover_type = self.options.crossover_type # Cruzamiento
+
+        self.mutation_type = self.options.mutation_type # Mutacion
+
+        self.mutation_prob = self.options.mutation_prob # Probabilidad de mutacion
+
+        self.selection_strategy = self.options.selection_strategy # Estrategia de seleccion de la nueva poblacion
+
+        self.gselection_type = self.options.gselection_type # Tipo de seleccion de la poblacion
 
         print(f"{bcolors.HEADER}\nIniciando Algortimo Genetico...{bcolors.ENDC}")
 
@@ -81,12 +93,72 @@ class GeneticAlgorithm():
         
         parents = []
         # Inicializar poblacion
-        print(f"{bcolors.BOLD}Generando poblacion inicial...{bcolors.ENDC}{bcolors.ENDC}")
-        
+        print(f"{bcolors.BOLD}Generando poblacion inicial...{bcolors.ENDC}")
         population = Population(pop_size=self.pop_size, problem=self.problem)
-        population.orderPopulation()
-        for i in population.pop:
-            print(i.cost)
+        # Iniciar poblacion de hijos
+        offspring = Population(problem=self.problem)
+        #population.printPop()
+        
+        i =0 
+        for tour in population.pop:
+            print(tour.cost, i)
+            i+=1
+
+        # Imprimir mejor solución encontrada 
+        print(f"{bcolors.UNDERLINE}Mejor individuo de la poblacion{bcolors.ENDC}")
+        population.getBestTour().printSol()
+        
+        # Guardar la mejor solución en best_tour
+        if not self.best_tour:
+            self.best_tour = Tour(tour=population.getBestTour())
+        else:
+            self.best_tour.copy(population.getBestTour())
+  
+        # tiempo para iteraciones y condicion de termino por tiempo
+        start = end = timer()
+
+        # bucle principal de Algoritmo Genetico
+        print(f"{bcolors.HEADER}\nComenzando Busqueda con Algoritmo Generico...\n{bcolors.ENDC}")
+
+        # Bucle principal del algoritmo
+        while (self.terminationCondition(self.iterations, self.evaluations, end-start)):
+            
+            end = timer() # tiempo actual de iteracion
+            i = 0
+            # Aplicar cruzamiento para generar poblacion de hijos
+            while (offspring.pop_size < self.offspring_size):
+                parents = population.selectParents(self.pselection_type)
+                
+                print(parents, i)
+                i+=1
+                offspring.pop_size += 1
+                
+
+          
+            # Actualizar contadores de iteraciones y evaluaciones, luego limpiar la poblacion de hijos
+            self.evaluations += self.offspring_size
+            self.iterations += 1
+            offspring.clear()
+
+
+    def terminationCondition(self, iterations: int, evaluations: int, time: float) -> bool:
+        """ Condicion de termino para el ciclo principal de Algoritmo Genetico, 
+        basado en los criterios de iteraciones, evaluaciones y tiempo, devuelve verdadero o falso si se debe continuar o no"""
+        # Criterio de termino de las iteraciones
+        if (self.options.max_iterations > 0):
+            if (iterations > self.options.max_iterations):
+                return False
+        # Criterio de termino de las evaluciones
+        if (self.options.max_evaluations > 0):
+            if (evaluations > self.options.max_evaluations):
+                return False
+
+        # Criterio de termino por tiempo
+        if (self.options.max_time > 0):
+            if (time > self.options.max_time):
+                return False
+        
+        return True
 
     def updateTrajectory(self) -> None:
         """ Actualiza el registro de mejores soluciones con todas las caracteristicas de su ejecución """
--- src/Algorithms/Population.py
+++ src/Algorithms/Population.py
@@ -1,20 +1,23 @@
+from src.AlgorithmsOptions import SelectionType
+from src.Utilities import bcolors, random
 from src.Tour import Tour
 from src.TSP import TSP
-from typing import Union
 
 class Population():
 
-    # instancia del tsp
-    problem: TSP
-	# Poblacion
-    pop = []
-	# tamaño poblacion
-    pop_size = 0
-	# indice del mejor_individuo 
-    best_index = -1
-	
+    
     def __init__(self, **kwargs) -> None:
-        
+
+        # instancia del tsp
+        self.problem: TSP
+        # Poblacion
+        self.pop = []
+        # tamaño poblacion
+        self.pop_size = 0
+        # indice del mejor_individuo 
+        self.best_index = -1
+
+
         # inicializar problema
         if ('problem' in kwargs):
             self.problem = kwargs['problem']
@@ -69,6 +72,7 @@ class Population():
             self.pop.append(indivi)
             self.pop_size += 1
         elif isinstance(indivi, list): # si es una lista de individuos
+            print(self.best_index, len(self.pop), indivi)
             self.pop.extend(indivi)
             self.pop_size += len(indivi)
 
@@ -86,12 +90,219 @@ class Population():
         self.pop_size = 0
         self.best_index = -1
 
+
     def searchBest(self) -> None:
         """ Busca el mejor individuo en la poblacion """
         # Encontrar y guardar el indice de el individuo de la poblacion con menor costo
-        self.best_index = self.pop.index( min(self.pop, key=lambda tour: tour.cost) )
-        
+        if self.pop:
+            self.best_index = self.pop.index( min(self.pop, key=lambda tour: tour.cost) )
+
+
     def orderPopulation(self) -> None:
         """Ordena la poblacion de menor a mayor fitness """
         self.pop.sort(key=lambda tour: tour.cost)
         self.searchBest()
+
+    
+    # Impresiones
+    def printPop(self) -> None:
+        """Imprime la poblacion de soluciones i costo"""
+        for i in range(self.pop_size):
+            print(f"{bcolors.UNDERLINE}Individuo {i+1}{bcolors.ENDC}")
+            self.pop[i].printSol()
+
+    """ METODOS VARIOS """
+
+    def getBestTour(self) -> Tour:
+        """Retorna el mejor individuo de la poblacion"""
+        if (self.best_index > -1):
+            return self.pop[self.best_index]
+        return None
+
+    def generateRouletteWheel(self, candidates: list) -> list:
+        """ Retorna la torta de probabilidades asociadas al fitness de las soluciones entregadas, la seleccion es 
+        proporcional al fitness
+        Ruleta para minimizacion: p(x1) = (min + max - f(x1)) / sum(f(x))
+
+        Returns
+        -------
+            list[float]
+                lista que posee las probabilidades acumuladas para la seleccion de soluciones
+        """
+        su = 0.0
+        # Encontrar el minimo y el máximo en los individuos
+        mini = (min(candidates, key=lambda tour: tour.cost)).cost
+        maxi = (max(candidates, key=lambda tour: tour.cost)).cost
+        roulette = [0.0] * len(candidates)
+
+        # Generar ruleta
+        for i in range(len(candidates)):
+            roulette[i] = float(mini + maxi - candidates[i].cost)
+            su += roulette[i]
+
+        roulette[0] /= su
+        for i in range(1, len(candidates)):
+            roulette[i] /= su
+            roulette[i] += roulette[i-1]
+
+        return roulette
+
+
+    """ METODOS DE SELECCION DE PADRES (2) """
+
+    def selectParents(self, stype: SelectionType) -> list:
+        """ Selecciona 2 padres segun el tipo de seleccion recibido por parametro 
+         
+            Returns
+            -------
+            list[int]
+                lista con los indices de los padres seleccionados
+        """
+        parents = []
+
+        if (stype == SelectionType.BEST):
+            parents = self.selectIBest(2)
+        elif (stype == SelectionType.RANDOM):
+            parents = self.selectIRandom(2)
+        elif (stype == SelectionType.ROULETTE):
+            parents = self.selectIRoulette()
+        elif (stype == SelectionType.TOURNAMENT):
+            parents = self.selectITournament()
+        else: 
+            parents = self.selectIBest(2)
+        
+        return parents
+
+    def selectIBest(self, size: int = 2) -> list:
+        """Selecciona la cantidad de individuos recibida por parametros (por defecto 2) en base al fitness, siendo una seleccion elitista
+            
+            Returns
+            -------
+            list
+                lista con los indices de los individuos seleccionados
+        """
+        sel = []
+        # Si se seleccionan mas individuos de los permitidos
+        if (size > self.pop_size):
+            print(f"{bcolors.FAIL}Error: No es posible seleccionar {size} de una poblacion con {self.pop_size} individuos{bcolors.ENDC}")
+            exit()
+ 
+        # Si todos son seleccionados
+        if (size == self.pop_size):
+            sel = list(range(size))
+            return sel
+        
+        # Ordenar poblacion
+        self.orderPopulation()
+        # Seleccionar
+        sel = list(range(size))
+
+        return sel
+
+    
+    def selectIRandom(self, size: int = 2) -> list:
+        """Selecciona individuos aleatoriamente (por defecto 2)
+         
+            Returns
+            -------
+            list
+                lista con los indices de los individuos seleccionados
+        """
+        sel = [] 
+        elegible = []
+        # Si se seleccionan mas individuos de los permitidos
+        if (size > self.pop_size):
+            print(f"{bcolors.FAIL}Error: No es posible seleccionar {size} de una poblacion con {self.pop_size} individuos{bcolors.ENDC}")
+            exit()
+        
+        # Si todos son seleccionados
+        if (size == self.pop_size):
+            sel = list(range(size))
+            return sel
+
+        # Indices de los candidatos que pueden ser seleccionados
+        elegible = list(range(self.pop_size))
+        # Seleccionar los individuos aleatoriamente desde los candidatos y del tamaño recibido
+        sel = random.sample(elegible, size)
+
+        return sel
+
+    def selectIRoulette(self, size: int = 2) -> list:
+        """Selecciona los individuos padres en base a la ruleta (por defecto 2)
+
+            Returns
+            -------
+            list
+                lista con los indices de los individuos seleccionados
+        """
+        candidates = []
+        ids = []
+        sel = []
+        roulette = []
+        r = 0.0
+
+        # Si se seleccionan mas individuos de los permitidos
+        if (size > self.pop_size):
+            print(f"{bcolors.FAIL}Error: No es posible seleccionar {size} de una poblacion con {self.pop_size} individuos{bcolors.ENDC}")
+            exit()
+        
+        # Si todos son seleccionados
+        if (size == self.pop_size):
+            sel = list(range(size))
+            return sel
+        # Anadir candidatos a ser seleccionados y sus ids
+        candidates.extend(self.pop)
+        ids = list(range(len(self.pop)))
+
+        # Seleccionar individuos
+        for _ in range(size):
+            # Generar ruleta
+            roulette = self.generateRouletteWheel(candidates)
+            # Seleccionar
+            r = random.random()
+            for i in range(len(candidates)):
+                if (r < roulette[i]):
+                    sel.append(ids[i])
+                    candidates.pop(i)
+                    ids.pop(i)
+                    break
+
+        return sel
+
+    def selectITournament(self, tsize: int = 3, size: int = 2) -> list:
+        """Selecciona individuos en base al fitness (por defecto 2) en un torneo (por defecto 3 participantes)
+
+            Returns
+            -------
+            list
+                lista con los indices de los individuos seleccionados
+        """
+        sel = []
+        tsel = []
+        # Si se seleccionan mas individuos de los permitidos
+        if (size > self.pop_size):
+            print(f"{bcolors.FAIL}Error: No es posible seleccionar {size} de una poblacion con {self.pop_size} individuos{bcolors.ENDC}")
+            exit()
+        
+        # reducir el tamaño del torneo si es necesario
+        while (tsize > self.pop_size):
+            tsize -= 1
+    	
+        # Si todos son seleccionados
+        if (size == self.pop_size):
+            sel = list(range(size))
+            return sel
+
+        # Seleccionar individuos
+        while (len(sel) < size):
+            # seleccionar individuos torneo
+            tsel = self.selectIRandom(tsize)
+
+            # Seleccionar y añadir mejor individuo de los individuos aleatorios del torneo
+            sel.append( min(tsel, key=lambda i: self.pop[i].cost) )
+            # convertir a set y luego a lista nuevamente para eliminar en caso de que hallan elementos repetidos
+            sel = list( set(sel) )
+            
+        return sel
+    	
+  
